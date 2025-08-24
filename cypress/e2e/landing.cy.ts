4da7daff8a6ab4e4989992efb2d8a05a0e2d30ea
lmbsumatra <122442812+lmbsumatra@users.noreply.github.com>
2025-08-25 00:04:30 +0800

    :: add tsconfig.json on cypress

--- cypress/e2e/landing.cy.ts
+++ cypress/e2e/landing.cy.ts
@@ -1,7 +1,12 @@
+/// <reference types="cypress" />
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('/')
     cy.contains("Short Stories")
 
+  })
+  it('passes',()=> {
+    cy.get('button') 
   })
 })
